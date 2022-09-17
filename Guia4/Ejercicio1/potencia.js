973149d7ce70c6111cb2815cd4b3ec6b48bd2ddc
CarlosAlvaZ <carlos.alvarenga017@gmail.com>
2022-09-17 16:17:24 -0600

    Ejercicio 1 guia3 finalizado

--- Guia4/Ejercicio1/potencia.js
+++ Guia4/Ejercicio1/potencia.js
@@ -2,3 +2,24 @@ const base = document.querySelector("#base")
 const exp = document.querySelector("#exp")
 const button = document.querySelector("#elevar")
 const target = document.querySelector("#resultTarget")
+
+function raise(base, exponente){
+    this.base = base
+    this.exponente = exponente
+    this.resultado = this.base ** this.exponente
+
+    this.getResult = ()=>{ return this.resultado}
+
+    this.print = (target)=>{
+        target.innerHTML = " "
+        let text = document.createTextNode(this.getResult())
+        target.appendChild(text)
+    }
+}
+
+button.addEventListener('click', ()=>{
+    if(base.value != 0 && exp.value !=0){
+        let aux = new raise(base.value, exp.value)
+        aux.print(target)
+    }
+})
