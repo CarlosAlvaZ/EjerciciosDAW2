e08c9dd531de3fe29321b6cd06c5f98b3b9bb81f
CarlosAlvaZ <carlos.alvarenga017@gmail.com>
2022-09-17 15:59:42 -0600

    Html y Css listo en Ejercicio1

---
+++ Guia4/Ejercicio1/potencia.js
@@ -0,0 +1,4 @@
+const base = document.querySelector("#base")
+const exp = document.querySelector("#exp")
+const button = document.querySelector("#elevar")
+const target = document.querySelector("#resultTarget")
